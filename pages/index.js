513b7d63bf587032f230ce98fdc6fb9e399e11be
Pelumi Oyesanya <pelumi351@gmail.com>
2022-05-21 14:27:43 +0100

    styled-up

--- pages/index.js
+++ pages/index.js
@@ -59,20 +59,38 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <div className='bg-[#011E30] flex flex-col min-h-screen'>
       <Head>
         <title>0-HELLO-WORLD</title>
         <meta name="description" content="Used by Emerald Academy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>User Address: {user.loggedIn ? user.addr : null}</h1>
-      <button onClick={fcl.authenticate}>Log In</button>
-      <button onClick={fcl.unauthenticate}>Log Out</button>
-      <button onClick={getGreeting}>Get Greeting</button>
-      <input type="text" onChange={e => setNewGreeting(e.target.value)} />
-      <button onClick={changeGreeting}>Change Greeting</button>
-      <h1>Greeting: {greeting}</h1>
+      <main className='container mx-auto flex-1 p-5'>
+        <div className='mb-10 flex justify-between items-center pr-10 pt-2'>
+          <h1 className={styles.sooth}>HELLO-WORLD</h1>
+          <div className='flex space-x-4 items-center'>
+            <h1 className='text-[#38E8C6]'>Address: </h1>
+            <h1 className='border px-7 text-center text-[#38E8C6] text-sm py-1 rounded-xl border-[#38E8C6] w-56'>{user.loggedIn ? user.addr : "Please connect wallet -->"}</h1>
+          </div>
+          <div>{!user ? <button className='border rounded-xl border-[#38E8C6] px-5 text-sm text-[#38E8C6] py-1' 
+            onClick={fcl.authenticate}>connect</button> : <button className='border rounded-xl border-[#38E8C6]
+            px-5 text-sm text-[#38E8C6] py-1' onClick={fcl.unauthenticate}>Logout</button>}
+          </div>
+        </div>
+        <div className='flex items-center justify-center pt-28'>
+          <div className='space-y-5 p-2 w-1/3'>
+              <button onClick={getGreeting} className="border rounded-lg py-2 text-sm px-5 border-[#38E8C6] text-blue-900 font-bold bg-[#38E8C6]">Get Greeting</button>
+              <div className='h-28 bg-[#00344B] border flex items-center justify-center text-white border-[#38E8C6] rounded-lg'>{greeting}</div>
+              <div className='flex flex-col space-y-3'>
+                <input type="text" placeholder='Say hello' className='px-4 py-1 focus:outline-none focus:border-[#38E8C6] focus:border-2 bg-green-100 border rounded-lg border-[#38E8C6]' onChange={e => setNewGreeting(e.target.value)} />
+                <button className='rounded-lg text-center text-sm font-bold text-blue-900 py-2 bg-[#38E8C6]' onClick={changeGreeting}>Change Greeting</button>
+              </div>
+          </div>
+        </div>
+      </main>
+      <footer className='container mx-auto'>
+        <img src='/city.svg' alt='city'/>
+      </footer>
     </div>
   )
 }
